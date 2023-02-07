d364dd59b055bd370bbfa08e480801c41e77f476
Aditya <adityasinghboss1234@gmail.com>
2023-02-07 16:55:09 +0530

    upload post done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cookieParser = require("cookie-parser");
 
 if(process.env.NODE_ENV !== "production") {
     require("dotenv").config({path: "backend/config/config.env"})    
@@ -8,13 +9,14 @@ if(process.env.NODE_ENV !== "production") {
 //Using Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 //Importing Routes 
 const post = require("./routes/post");
 const user = require("./routes/user");
 
 //Using Routes
-app.use("api/v1", post);
+app.use("/api/v1", post);
 app.use("/api/v1", user);
 
 module.exports = app;
